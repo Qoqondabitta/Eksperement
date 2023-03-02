7e1b050be631ad45286cd424f483d6ae4f821c6f
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-02 19:11:15 +0500

    works

---
+++ 5-lesson/style.js
@@ -0,0 +1,123 @@
+//if ("0") {
+//    alert('Hello');
+//}
+//================================================================================================================
+//let a="ECMAScript";
+//
+//if (a === "ECMAScipt") {
+//    console.log("right");
+//} else {
+//    console.log(`You don't know "ESCMAScript"!`);
+//}
+//===================================================================================================================
+//let a=-10;
+//a>0 ? console.log("1"): console.log("no");
+//let a=-10;
+//a<0 ? console.log("-1"): console.log("no");
+//let a=0;
+//a===0 ? console.log('0'): console.log('no');
+//==============================================================================================================
+//let a=2;
+//let b=1
+//let result;
+//
+//if (a + b < 4) {
+//    console.log(result='Below');
+//} else {
+//    console.log(result='Over');
+//}
+//===================================================================================================================
+//let message;
+//let login='Director'//
+//login=='Employee' ? console.log(message = "Hello"): login == 'Director' ? console.log(message = 'Greetings'): login == '' ? console.log(message = 'No login'): console.log(message = '');
+//===================================================================================================
+//let name='Abdulakhad';
+//
+//console.log(`HEllo ${1}`); // Hello 1
+//
+//console.log(`Hello ${"name"}`); // Hello name
+//
+//console.log(`HEllo ${name}`) // Hello Abdulakhad
+//==============================================================================================================
+//let a = 1, b = 1;
+//
+//let c = ++a; // 2
+//let d = b++ // 1
+//
+//console.log(c, d);
+//=================================================================================================================
+//let a = 2;
+//
+//let x = 1 + (a *= 2);
+//
+//console.log(x); // 5
+//===============================================================================================================
+//console.log("" + 1 + 0); // 10
+//console.log("" - 1 + 0); // -1
+//console.log(true + false); // 1
+//console.log(6 / "3"); // 2
+//console.log("2" * "3"); // 6
+//console.log(4 + 5 + "px"); // 9px
+//console.log("5" + 4 + 5); // 545
+//console.log("4" - 2); // 2
+//console.log("4px" - 2); // NaN
+//console.log(" -9 " + 5); // -9 5
+//console.log(null + 1); // 1
+//console.log(undefined + 1); // NaN
+//console.log("\t\n" -2); // -2
+//=================================================================================================================
+//console.log(5 > 4); // true
+//console.log("apple" > "pineapple"); // false
+//console.log("2" > "12"); // true
+//console.log(undefined == null); // true
+//console.log(undefined === null); // false
+//console.log(null == "\n0\n"); // false
+//console.log(null === +"\n0\n"); //false
+//===============================================================================================================
+//let browser='Yandex';
+//
+//switch (browser) {
+//    case 'Edge':
+//       alert("You have got the edge!");          
+//       break
+//       case 'Chrome':
+//       case 'Firefox':
+//       case 'Safari':
+//       case 'Opera':
+//            alert('Okay we support these browsers too')   
+//       default:
+//        alert("We hope that this page looks ok!");            
+//}
+//==============================================================================================================
+//let a = +prompt('a?', '')
+//
+//if (a == 0) {
+//    alert( 0 );
+//}
+//
+//if (a == 1) {
+//    alert( 1 )
+//}
+//
+//if (a == 2 || a == 3) {
+//    alert('2,3');
+//}
+//====================================================================================================
+//let i = 3;
+//
+//while (i) {
+//    alert(i--); //3
+//}
+//======================================================================================================
+//let i=1;
+//
+//for (i = 1; i < 9; i++) {
+//    console.log(i);                  
+//}
+//============================================================================================================
+// let i=1;
+
+// for(let i=1; i <= 15; i++) {
+//     console.log(i);
+//     i +=i
+//}
