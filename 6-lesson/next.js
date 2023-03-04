d14fb339a498a81cf547eb1361d73fa0ec2f3943
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-04 11:22:16 +0500

    (do)while

---
+++ 6-lesson/next.js
@@ -0,0 +1,12 @@
+//while (1) {
+//    console.log('Sarvar');
+//    console.log('Abdulloh aka');
+//    continue
+//    console.log('me');     
+//}
+//====================================
+//for ( i = 1; i < 4; i++){    
+//   for( j = 3; j > 1; j--)
+//   console.log(j + i); 
+//   
+//}
