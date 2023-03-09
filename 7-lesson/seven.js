9686198bbe12425e968e6dcad45213a4333991ff
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-09 11:38:59 +0500

    connecting two distinctive objects

--- 7-lesson/seven.js
+++ 7-lesson/seven.js
@@ -1,9 +1,64 @@
-let obj = {
-    name : `John`,
-    surname : `Smith`,
-    name : `Pete`
-}
-obj.age = 17,
-delete obj.name
-console.log(obj);
-console.log(Object.values(obj)); 
+// let obj = {
+//     name : `John`,
+//     surname : `Smith`,
+//     name : `Pete`
+// }
+// obj.age = 17,
+// delete obj.name
+// console.log(obj);
+// console.log(Object.values(obj)); 
+//=================================================
+// function dec(a) {
+//     return a;
+// }
+
+// console.log(dec('man'));
+//====================================================
+// let obj = {
+//     name : 'Abdulahad',
+//     Age : 17
+// }
+
+// let user = obj
+
+// // console.log(user);
+// console.log(user==obj);
+// console.log(user=== obj);
+// let obj = {
+//     name : 'Abdulahad',
+//     Age : 17
+// }
+
+// let user = structuredClone(obj);
+
+// obj.name = 'man'
+
+// console.log(user);
+// console.log(obj);
+// console.log(user==obj);
+// console.log(user=== obj);
+//=====================================================
+// let obj = {
+//     name : 'Abdulahad',
+//     Age : 17
+// }
+
+// let user = {
+//     info : 'boy'
+// }
+
+// console.log(Object.assign(obj, user));
+//=========================================================
+// let obj = {
+//     name : 'Abdulahad',
+//     Age : 17
+// }
+
+// let user = {
+//     info : 'boy'
+// }
+
+// obj = {...obj, ...user}
+
+// console.log(obj);
+//==================================================
