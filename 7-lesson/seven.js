8a6e9d8f06e80ee70bc06fcc12e47784236591f4
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-09 16:10:37 +0500

    Objects homework finish

--- 7-lesson/seven.js
+++ 7-lesson/seven.js
@@ -130,3 +130,53 @@
 
 // console.log(obj);
 //================================================
+// let adn = {
+//     b : 200,
+//     c : 300,
+//     d : 100
+// }
+// let e = 0;
+// for (i in adn) {
+//     e += adn[i]
+// }
+// console.log(e);
+// console.log(adn);
+//===================================================
+// let obj = {
+//     a : 200,
+//     b : 300,
+//     c : 'hi'
+// }
+
+// let d = 0;
+// for (i in obj) {
+// if (typeof obj[i] == 'number') {
+//     console.log(obj[i] * 2);
+// } 
+// if (typeof obj[i]=="string") {
+//     delete obj[i]
+// }
+// }
+//=========================================
+// let obj = {
+//     a : 3
+// }
+
+// let user = {
+//     b : 5
+// }
+
+// console.log(Object.assign(obj, user));
+// console.log(obj == user);
+// console.log(obj === user);
+let obj = {
+        a : 3
+    }
+    
+    let user = {
+        b : 5
+    }
+    let f = Object.assign(obj)
+    console.log(f);
+    console.log(obj == f);
+    console.log(obj === user);
