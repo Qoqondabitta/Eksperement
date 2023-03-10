eb49daa79f0b17ded9c29f7bed8f92cd6101ad48
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-10 11:58:51 +0500

    function insife the object

---
+++ 8-lesson/eight.js
@@ -0,0 +1,62 @@
+// function it() {
+//     console.log('he');
+// }
+
+// let a = {
+//     name : 'me',
+//     it () {
+//         console.log('he');
+//     }
+// }
+
+// it()
+// a.it()
+//===========================================================
+// function adn () {
+//     this.name = 'he';
+//     // console.log(this);
+// }
+
+// console.log(new adn().name);
+//==================================================
+// function a () {
+//     console.log('Hi');
+// }
+
+// let b = {
+//     name : 'it',
+//     f () {
+//       console.log('Shukur');
+//     }
+// }
+
+// a()
+// console.log(b);
+// console.log(Object.keys(b));
+// b.f()
+//=================================================
+// function a () {
+//     console.log('Hi');
+// }
+
+// let b = {
+//     name : 'it',
+//     a,
+// }
+
+// a()
+// b.a()
+//=================================================
+// let a = {
+//     d : 'c',
+//     b () {
+//         console.log(`Third is ${a.d}`);
+//         console.log(`Third is ${this.d}`);
+//     }
+// }
+
+// let f = a
+// a = null
+
+// console.log(f);
+//============================================
