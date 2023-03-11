869fee0f54fe162109d3525b25cfc42bc4fee76c
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-11 09:28:59 +0500

    object advanced

--- 8-lesson/eight.js
+++ 8-lesson/eight.js
@@ -50,13 +50,70 @@
 // let a = {
 //     d : 'c',
 //     b () {
-//         console.log(`Third is ${a.d}`);
-//         console.log(`Third is ${this.d}`);
+//         console.log( `Third is ${a.d}` );
+//         console.log( `Third is ${this.d}` );
 //     }
 // }
 
-// let f = a
-// a = null
+// let f = a;
+// a = null;
 
-// console.log(f);
+// f.b()
 //============================================
+// let id1 = Symbol('he')
+
+// let obj = {
+//     [id1] : 'she'
+// }
+
+// console.log(obj[id1]); 
+//===================================================
+// let id1 = Symbol('he')
+// let id2 = Symbol('he')
+
+// let obj = {
+//     [id1] : 'she',
+//     ['id2'] : 'iy'
+// }
+
+// console.log(obj[id2]); 
+// console.log(obj['id2']); 
+//===========================================================
+// let a ={
+//     b : 'c',
+//     d : 'f',
+//     e : 'g'
+// }
+
+// for (value in a) {
+//     console.log(value);
+// }
+//==================================
+// for (value in a) {
+//     console.log(value);
+// }
+// let a ={
+//     b : 'c',
+//     d : 'f',
+//     e : 'g'
+// }
+
+// const {b, d, e} = a
+
+// console.log(d);
+//===========================================
+// let b ='m';
+
+// let a ={
+//     b : 'c',
+//     d : 'f',
+//     e : 'g'
+// }
+// for (value in a) {
+//     // console.log(value);
+// }
+
+// const { b : newB, d, e } = a;
+
+// console.log(newB);
+// console.log(a.b);
