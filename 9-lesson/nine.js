4e17877f5bbb2aa60ffd21ae4b55fd8bd4d69aee
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-16 10:31:40 +0500

    string methods teaching in video

--- 9-lesson/nine.js
+++ 9-lesson/nine.js
@@ -9,37 +9,40 @@
 // console.log(a[6]);
 // console.log(a[7]);
 // console.log(a[8]);
-//==============================================================================
+//====================================================================
 // let a = `I love me`;
 // let b = `me`
 
 // console.log(a[-1]);
 // console.log(b.charAt(1));
 // console.log(a.at(-1));
-//==============================================================================
-let a = `I Love me and I`;
-let b = `me`
-let c = `+998 (90) 111-65-54`
-let d = `12345678`
-let e = '3+6-7'
-let f = ' 234'
+//=====================================================================
+// let a = `I Love me and I`;
+// let b = `me`
+// let c = `+998 (90) 111-65-54`
+// let d = `12345678`
+// let e = '3+6-7'
+// let f = ' 234'
 
-console.log(b[b.length -2]);
-console.log(b.charAt(2));
-console.log(b.at(-0));
-console.log(b.toUpperCase(3));
-console.log(a.toLowerCase);
-console.log(a.indexOf('I', 1));
-console.log(c.includes('65'));
-console.log(c.startsWith('+998'));
-console.log(c.endsWith('54'));
-console.log(d.slice(1,-5));
-console.log(d.substring(1,7));
-console.log(d.substr(1,5));
-console.log(eval(e));
-console.log(f.trim().length);
-console.log(f.trimEnd().length);
-console.log(f.trimStart().length);
+// console.log(b[b.length -2]);
+// // console.log(a.split(a.toUpperCase(0, 2, 7)));
+// console.log(b.charAt(2));
+// console.log(b.at(-0));
+// console.log(b.toUpperCase(3));
+// console.log(a.toLowerCase);
+// console.log(a.indexOf('I', 1));
+// console.log(a.lastIndexOf('I', 7));
+// console.log(c.includes('65'));
+// console.log(c.startsWith('+998'));
+// console.log(c.endsWith('54'));
+// console.log(d.slice(1,-5));
+// console.log(d.substring(1,7));
+// console.log(d.substr(1,5));
+// console.log(eval(e));
+// console.log(f.trim().length);
+// console.log(f.trimEnd().length);
+// console.log(f.trimStart().length);
+// console.log(a.toLocaleUpperCase(0, 2, 7));
 
 
 
@@ -53,3 +56,38 @@ console.log(f.trimStart().length);
 // tel('909621180')
 // tel('930088217')
 // tel('930067413')
+//===================================================================
+let b = 'a'
+let a = `Abdulahad and Sardor and Anvar`
+
+// console.log(a);
+// console.log(b[3]);
+// console.log(b.charAt(3));
+// console.log(b.at(-3));
+// console.log(a[a.length-3]);
+// console.log(b.toUpperCase(2));
+// console.log(b.toLowerCase(0));
+// console.log(a.includes('as'));
+// console.log(a.startsWith('Ab'));
+// console.log(a.endsWith('r'));
+// function telephone(number) {
+//     console.log(number.padStart(19, '+998'));
+// }
+
+// telephone(' (90) 111-65-54')
+// telephone(' (90) 962-11-80')
+// console.log(a.slice(4, -1));
+// console.log(a.substring(28, 25));
+// console.log(a.substr(0, 6));
+// console.log(eval('3+6-7'));
+// console.log(b.trim()); // b.trimStart/End
+// console.log(b.split(''));
+// console.log(b.concat('against', ' versus'));
+// console.log(b.search(/4d/i));
+// for (i in b) {
+//     console.log(i);
+// }
+// for (i of b) {
+//     console.log(i);
+// }
+// console.log(b.localeCompare('A'));
