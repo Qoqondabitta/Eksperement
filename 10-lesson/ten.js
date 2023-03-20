cbbcd2b0fc976da28d7d761b71e39ecbf9f84dc9
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-20 19:04:19 +0500

    array beginning

---
+++ 10-lesson/ten.js
@@ -0,0 +1,34 @@
+// let a = [1,  'me', 10, true, false]
+
+// // console.log(a.slice(1));
+// // let b = a.splice(2)
+// a.pop()
+// a.pop()
+// console.log(a.splice(1, 1, 'can wear number'));
+// console.log(a);
+//==============================
+// let a = [1,  'me', 10, true, false];
+
+// console.log(a.indexOf(true));
+let a = [1, 9, 10, 5] 
+let b =''
+
+// a.join('+')
+b += a
+// console.log(b);
+// console.log(a.join('+'));
+console.log(eval(a.join('+')));
+// function calculate(a) {
+//     let b = '';
+//     b += a
+//     // console.log(a.join('+'));
+//     // console.log(eval(b)); 
+//     console.log(b);   
+//     return b;
+// }
+
+// console.log(calculate(b));
+// calculate()
+// console.log(a);
+// calculate(eval(a))
+// console.log(calculate());
