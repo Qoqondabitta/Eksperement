6ea924abd1ee7b0ef453fef75b0c905b4508f7ec
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-25 13:06:26 +0500

    array second part homework

--- 11-lesson/elven.js
+++ 11-lesson/elven.js
@@ -15,6 +15,38 @@
 // let filter = cars.sort((a,b)=> a.year-b.year)
 // let filter = cars.sort((a,b)=>a.engine-b.engine)
 // let filter = cars.sort((a,b)=> a.brand.localeCompare(b.brand))
-let filter = cars.filter((value)=> value.year < 2000 )
+// let filter = cars.filter((value)=> value.year < 2000 )
 // filter.value.push(endi=`eski`)
-console.log(filter.push(endi=`eski`));
+// console.log(filter.push(endi=`eski`));
+// 2010 - 2022 yillarda chiqgan mashinalarga status : "Yangi" degan malumot qoshing ?
+
+const model = (data) => {
+  let res = data.map((value) =>
+    value.year < 2022 && value.year > 2010
+      ? { ...value, status: "Yangi" }
+      : value
+  );
+
+  cars = res;
+};
+model(cars);
+
+const model1 = (data) => {
+  let res = data.map((value)=>value.year <= 2000
+  ? {...value, status: `Eski`} 
+  : value
+  );
+  cars = res
+}
+model1(cars);
+
+const model2 = (data) => {
+  let res = data.map((value)=> value.year > 2000 && value.year <= 2010 
+  ? {...value, status: `Norm`}
+  : value
+  )
+  cars = res
+}
+model2(cars)
+console.log(cars);
+
