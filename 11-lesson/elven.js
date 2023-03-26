7c096957fd9826984e60e4ace143aaae1072a121
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-26 23:26:40 +0500

    new date

--- 11-lesson/elven.js
+++ 11-lesson/elven.js
@@ -8,7 +8,7 @@
     {id:7, year:2018, engine:2.4, brand:`Tracker`}
 ]
 
-// let filter = cars.filter((value)=>value.year < 2000)
+// let filter = cars.filter((value)=>value.year < 2010)
 // let filter = cars.filter((value)=>value.year > 2010)
 // let filter = cars.filter((value)=> value.year < 2010)
 // let filter = cars.filter((value)=> value.year == 2010)
@@ -17,36 +17,75 @@
 // let filter = cars.sort((a,b)=> a.brand.localeCompare(b.brand))
 // let filter = cars.filter((value)=> value.year < 2000 )
 // filter.value.push(endi=`eski`)
-// console.log(filter.push(endi=`eski`));
+// console.log(filter);
 // 2010 - 2022 yillarda chiqgan mashinalarga status : "Yangi" degan malumot qoshing ?
 
-const model = (data) => {
-  let res = data.map((value) =>
-    value.year < 2022 && value.year > 2010
-      ? { ...value, status: "Yangi" }
-      : value
-  );
+// const model = (data) => {
+//   let res = data.map((value) =>
+//     value.year < 2022 && value.year > 2010
+//       ? { ...value, status: "Yangi" }
+//       : value
+//   );
 
-  cars = res;
-};
-model(cars);
+//   cars = res;
+// };
+// model(cars);
 
-const model1 = (data) => {
-  let res = data.map((value)=>value.year <= 2000
-  ? {...value, status: `Eski`} 
-  : value
-  );
-  cars = res
-}
-model1(cars);
+// const model1 = (data) => {
+//   let res = data.map((value)=>value.year <= 2000
+//   ? {...value, status: `Eski`} 
+//   : value
+//   );
+//   cars = res
+// }
+// model1(cars);
 
-const model2 = (data) => {
-  let res = data.map((value)=> value.year > 2000 && value.year <= 2010 
-  ? {...value, status: `Norm`}
-  : value
-  )
-  cars = res
-}
-model2(cars)
-console.log(cars);
+// const model2 = (data) => {
+//   let res = data.map((value)=> value.year > 2000 && value.year <= 2010 
+//   ? {...value, status: `Norm`}
+//   : value
+//   )
+//   cars = res
+// }
+// model2(cars)
+// console.log(cars);
+// ========================================
+let a = [1, 2, 3, 4, 5]
+// console.log(a.find((value)=> value == 5));
+// console.log(a.findIndex((value)=> value == 4));
+// console.log(a.copyWithin(0, 2, 4));
+// console.log(a.fill('*', 2, 4));
+// ============================================
+// let b = [1, 2, [3, [4], 5], 6, 7]
+// console.log(b.flat());
+// console.log(b.flat(Infinity));
+// console.log(b.flat(3));
+// let res = b.every((value)=> value ==1)
+// let res1 = b.some((value)=> value ==1)
+// console.log(res);
+// console.log(res1);
+// ============================
+// let res3 = a.map((value)=>value + 2)
+// console.log(res3);
+// let next = a.forEach((value)=>value)
+// console.log(next);
+// ===============================
+// console.log(Array.from('web', (v)=>v.toUpperCase()));
+// ================================
+// let sum = 0
+// let res2 = a.map((value)=>sum += value)
+// console.log(sum);
 
+// let and = a.reduce((value, curent)=>value + curent,0)
+// console.log(and);
+let data = new Date();
+
+console.log(data.getTime());
+console.log(data.getDate());
+console.log(data.getDay());
+console.log(data.getMonth());
+console.log(data.getFullYear());
+console.log(data.getHours());
+console.log(data.getMinutes());
+console.log(data.getSeconds());
+console.log(data.getMilliseconds());
