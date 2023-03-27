e29128eca76ddc18b14f8e40c2f77214c9555bc3
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-27 18:06:09 +0500

    tasks homework

--- 11-lesson/elven.js
+++ 11-lesson/elven.js
@@ -1,12 +1,12 @@
-  let cars = [
-    {id:1, year:1998, engine:1, brand:`Tico`},
-    {id:2, year:2005, engine:1.2, brand:`Matiz`},
-    {id:3, year:2010, engine:1.6, brand:`Gentra`},
-    {id:4, year:2010, engine:1.5, brand:`Cobalt`},
-    {id:5, year:2012, engine:2, brand:`Malibu`},
-    {id:6, year:2000, engine:1.2, brand:`Damas`},
-    {id:7, year:2018, engine:2.4, brand:`Tracker`}
-]
+//   let cars = [
+//     {id:1, year:1998, engine:1, brand:`Tico`},
+//     {id:2, year:2005, engine:1.2, brand:`Matiz`},
+//     {id:3, year:2010, engine:1.6, brand:`Gentra`},
+//     {id:4, year:2010, engine:1.5, brand:`Cobalt`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Damas`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`}
+// ]
 
 // let filter = cars.filter((value)=>value.year < 2010)
 // let filter = cars.filter((value)=>value.year > 2010)
@@ -33,7 +33,7 @@
 
 // const model1 = (data) => {
 //   let res = data.map((value)=>value.year <= 2000
-//   ? {...value, status: `Eski`} 
+//   ? {...value, status: `Eski`}
 //   : value
 //   );
 //   cars = res
@@ -41,7 +41,7 @@
 // model1(cars);
 
 // const model2 = (data) => {
-//   let res = data.map((value)=> value.year > 2000 && value.year <= 2010 
+//   let res = data.map((value)=> value.year > 2000 && value.year <= 2010
 //   ? {...value, status: `Norm`}
 //   : value
 //   )
@@ -50,7 +50,7 @@
 // model2(cars)
 // console.log(cars);
 // ========================================
-let a = [1, 2, 3, 4, 5]
+// let a = [1, 2, 3, 4, 5]
 // console.log(a.find((value)=> value == 5));
 // console.log(a.findIndex((value)=> value == 4));
 // console.log(a.copyWithin(0, 2, 4));
@@ -78,14 +78,122 @@ let a = [1, 2, 3, 4, 5]
 
 // let and = a.reduce((value, curent)=>value + curent,0)
 // console.log(and);
-let data = new Date();
+// let data = new Date();
 
-console.log(data.getTime());
-console.log(data.getDate());
-console.log(data.getDay());
-console.log(data.getMonth());
-console.log(data.getFullYear());
-console.log(data.getHours());
-console.log(data.getMinutes());
-console.log(data.getSeconds());
-console.log(data.getMilliseconds());
+// console.log(data.getTime());
+// console.log(data.getDate());
+// console.log(data.getDay());
+// console.log(data.getMonth());
+// console.log(data.getFullYear());
+// console.log(data.getHours());
+// console.log(data.getMinutes());
+// console.log(data.getSeconds());
+// console.log(data.getMilliseconds());
+
+// HOMEWORK
+// const ten = (a) => b = a.filter((value)=> value>9 && value<100)
+// console.log(ten([2, 1, 3, 10, 60, 50, 30, 20, 101]));
+// =================================================================
+// let a = {
+//   b: "c",
+//   d: true,
+//   e: 17,
+//   f: false,
+//   g: true,
+// };
+// let h = "";
+// for (i in a) {
+//   if (a[i] === true || a[i] === false) {
+//     h += `${i}: ${a[i]} `;
+//   }
+// }
+
+// console.log(h);
+// ===========================================================
+
+// console.log(Object.values(a)===true || false);
+// function quantity(str) {
+//   let res = {};
+//   for (i of str) {
+//     if (res[i]) {
+//       res[i] = res[i] + 1;
+//     } else {
+//       res[i] = 1;
+//     }
+//   }
+//   console.log(res);
+// }
+
+// quantity(`webbrain`);
+// ========================================================
+// let a = 'AbdulaAkhaD';
+// let count = 0;
+// let count2 = 0;
+// let b = ""
+// let c= ''
+// for(i of a) {
+//     if (i === i.toUpperCase()) {
+//         count++
+//        b = `Katta harf - ${count}`
+//     } else {
+//         count2++
+//        c= `Kichkina harf - ${count2}`
+//     }
+
+// }
+
+// console.log(`${b}
+// ${c}`);
+// =================================================
+// let obj = {title: 'BRO', age: 6}
+
+// console.log(Object.keys(obj));
+// let a = "WebBr@in Ac@demy";
+// let b = "";
+// for (i of a) {
+//   if (i !== "@") {
+//     b += i;
+//   } else {
+//     b += "a";
+//   }
+// }
+
+// console.log(b);
+// let a = "webbrraaiin";
+
+// function duble(a) {
+//   let res2 = "";
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] !== a[i + 1]) {
+//       res2 += a[i];
+//     }
+//   }
+//   console.log(res2);
+// }
+// duble(a);
+// for (i of a) {
+//   if (b[i]) {
+//     b += "";
+//   } else {
+//     b += b[i];
+//   }
+// }
+// console.log(b);
+// console.log(a);
+// // let a = 'web'
+// // let b = 'bew'
+// console.log(res2);
+
+
+// function restrict (a) {
+//     let combain = []
+//     for(let i = 0; i<a.length;i++){
+//         if(a[i] !== a[i+1]){
+//             combain=[...combain, a[i]]
+//         }
+//     }
+//     console.log(combain);
+// }
+
+// restrict([1, 1, 1, 2, 3, 6, 6, 10])
+// ==========================================
