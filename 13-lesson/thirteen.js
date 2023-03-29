6df0912d31c0b49511d5eef8429ecd76626de79a
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-29 20:51:12 +0500

    new date homework done

---
+++ 13-lesson/thirteen.js
@@ -0,0 +1,10 @@
+// const dddd = (weekdays) => {
+// let date=  new Date();
+// console.log(weekdays[date.getDay()]);
+// }
+// dddd(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
+// ========================================
+const fullDate = (months)
+
+
+
