060e116931fb97b532adf58a8bc0059ee09c2357
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-03-30 12:35:52 +0500

    new date homework practice

--- 13-lesson/thirteen.js
+++ 13-lesson/thirteen.js
@@ -4,7 +4,22 @@
 // }
 // dddd(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
 // ========================================
-const fullDate = (months)
+// const fullDate = (months,sign,tick,letter) => {
+//     let date = new Date();
+//     console.log(`${months[date.getMonth()]}${sign}${date.getDate()}th${sign}${date.getFullYear()},${sign}${date.getHours()}${tick}${date.getMinutes()}${tick}${date.getSeconds()}${sign}${letter}`);
+// }
+// fullDate(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], ' ', ':', 'pm')
+// ===============================================
+// const short = (months,sign) => {
+//     let date = new Date();
+//     console.log(`${months[date.getMonth()].slice(0,3)}${sign}${date.getDate()}th${sign}${date.getFullYear()}`); 
+// }
+// short(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], ' ')
+// ============================================================
+// const hoursMinuts = (sign) => {
+//     let date = new Date();
+//     console.log(`${date.getHours()}${sign}${date.getMinutes()} PM`);
+// }
 
-
-
+// hoursMinuts(':')
+// ====================================================
