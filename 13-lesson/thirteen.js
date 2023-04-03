9290ffad1df4ae34703422936b2396b1db9dc513
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-03 21:35:41 +0500

    preparing for exam

--- 13-lesson/thirteen.js
+++ 13-lesson/thirteen.js
@@ -62,55 +62,81 @@
 // let a = 5.0035;
 
 // console.log(a.join());
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
 // =========================================
-// const getTime = (title) => {
-//     let time = new Date();
-//     let hours = time.getHours() % 12;
-//     let minuts = time.getMinutes();
-//     let seconds = time.getSeconds();
-//     let weekday = time.getDay();
-//     let date = time.getDate();
-//     let month = time.getMonth()+1;
-//     let year = time.getFullYear();
-//     let sign;
-//     let nameMonth = [
-//         'December',
-//         'January',
-//         'February',
-//         'March',
-//         'April',
-//         'May',
-//         'June',
-//         'July',
-//         'August',
-//         'September',
-//         'October',
-//         'November'
-//     ];
-//     let weekdays = [
-//         'Sunday',
-//         'Monday',
-//         'Tuesday',
-//         'Thurswday',
-//         'Wednesday',
-//         'Friday',
-//         'Saturday'
-//     ]; 
+const moment = () => {
+    moment.locale = (title) => {
+        let time = new Date();
+        let hours = time.getHours() % 12;
+        let minuts = time.getMinutes();
+        let seconds = time.getSeconds();
+        let weekday = time.getDay();
+        let date = time.getDate();
+        let month = time.getMonth()+1;
+        let year = time.getFullYear();
+        let sign;
+        let nameMonth = [
+            'December',
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November'
+        ];
+        let weekdays = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Thurswday',
+            'Wednesday',
+            'Friday',
+            'Saturday'
+        ]; 
+        
+        time.getHours() < 12 ? sign = 'AM' : sign = 'PM'
     
-//     time.getHours() < 12 ? sign = 'AM' : sign = 'PM'
-
-//     if (title === 'LT') return `${hours}:${minuts} ${sign}`;
-//     else if (title === 'LTS') return `${hours}:${minuts}:${seconds} ${sign}`
-//     else if (title === 'L') return `${month < 10 ? '0' + month : month}/${date < 10 ? '0' + date : date}/${year}`
-//     else if (title==='l') return `${month}/${date}/${year}`
-//     else if (title==='LL') return `${nameMonth[month]} ${date}, ${year}`
-//     else if (title==='ll') return `${nameMonth[month].slice(0,3)} ${date}, ${year}`
-//     else if (title==='LLL') return `${nameMonth[month]} ${date}, ${year} ${hours}:${minuts} ${sign}`
-//     else if (title==='lll') return `${nameMonth[month].slice(0,3)} ${date}, ${year} ${hours}:${minuts} ${sign}`
-//     else if (title==='LLLL') return `${weekdays[weekday]}, ${nameMonth[month]} ${date}, ${year} ${hours}:${minuts} ${sign}`
-//     else if (title==='llll') return `${weekdays[weekday].slice(0,3)}, ${nameMonth[month]} ${date}, ${year} ${hours}:${minuts} ${sign}`
-// }
-
+        if (title === 'LT') return `${hours}:${minuts} ${sign}`;
+        else if (title === 'LTS') return `${hours}:${minuts}:${seconds} ${sign}`
+        else if (title === 'L') return `${month < 10 ? '0' + month : month}/${date < 10 ? '0' + date : date}/${year}`
+        else if (title==='l') return `${month}/${date}/${year}`
+        else if (title==='LL') return `${nameMonth[month]} ${date}, ${year}`
+        else if (title==='ll') return `${nameMonth[month].slice(0,3)} ${date}, ${year}`
+        else if (title==='LLL') return `${nameMonth[month]} ${date}, ${year} ${hours}:${minuts} ${sign}`
+        else if (title==='lll') return `${nameMonth[month].slice(0,3)} ${date}, ${year} ${hours}:${minuts} ${sign}`
+        else if (title==='LLLL') return `${weekdays[weekday]}, ${nameMonth[month]} ${date}, ${year} ${hours}:${minuts} ${sign}`
+        else if (title==='llll') return `${weekdays[weekday].slice(0,3)}, ${nameMonth[month]} ${date}, ${year} ${hours}:${minuts} ${sign}`
+    }
+}
+moment()
+console.log(moment.locale('l'))
+// =============================================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
+// ===============================================
 // console.log(getTime('llll')); 
 // let a = [1,2,3,4]
 
@@ -118,19 +144,19 @@
 
 // console.log(b);
 // ==========================================
-let a = {
-    age:17
-}
+// let a = {
+    // age:17
+// }
 
-let b = new Map();
+// let b = new Map();
 
-a.name = 'me'
-b.name = 'he'
+// a.name = 'me'
+// b.name = 'he'
 
-b.set('name', 'myself')
+// b.set('name', 'myself')
 // console.log(b);
 // console.log(a);
 // console.log(a,b.name);
 // console.log(b,a.name);
 // console.log(b.size);
-console.log(b.get('name'));
+// console.log(b.get('name'));
