7b4af75052d58c31d97a2524b4f2df0febd1d69f
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-05 07:00:00 +0500

    recalling exam tasks

--- 9-lesson/nine.js
+++ 9-lesson/nine.js
@@ -336,23 +336,23 @@ counting('weeebbbbbraaiinn')
 
 // binary(`10001111111001`)
 //======================================================
-// function text(str) {
-//   let res = "";
-//   let count = 1;
-//   for (let i = 0; i < str.length; i++) {
-//     if (str[i] === str[i + 1]) {
-//       count++;
-//     } else {
-//       res += str[i];
-//       if (count > 1) {
-//         res += count;
-//         count = 1;
-//       }
-//     }
-//   }
-//   console.log(res);
-// }
-// text("weebbbrrrraaaaaiiiiiinnnnnnn");
+function text(str) {
+  let res = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      res += str[i];
+      if (count > 1) {
+        res += count;
+        count = 1;
+      }
+    }
+  }
+  console.log(res);
+}
+text("weebbbrrrraaaaaiiiiiinnnnnnn");
 //==================================================
 // function getBynary(num) {
 //     let bynar = num.toString(2);
