c380c03f5bad79d60130d4bca71bb49341e9208b
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-08 00:24:12 +0500

    rudiments of js

---
+++ 1-lesson/first.js
@@ -0,0 +1,27 @@
+// let a = 10
+// var v = 'web'
+// const c = true
+// let d = 121823748974298n
+// let s;
+// let k = null
+// let obj = {a:'b'}
+// let obj1 = {a:'b'}
+// let f = [1, 2]
+// let f1 = [1, 2]
+// const test = () => {console.log('hey');}
+// test()
+// console.log(a, v, c, d, s, k, obj, f);
+// console.log(typeof a);
+// console.log(typeof c);
+// console.log(typeof a);
+// console.log(typeof d);
+// console.log(typeof s);
+// console.log(typeof a);
+// console.log(typeof test);
+// console.log(typeof obj);
+// console.log(typeof k);
+// console.log(obj === obj1);
+// console.log(f === f1);
+// console.log(f == obj);
+// console.log(s == k);
+// console.log(s === k);
