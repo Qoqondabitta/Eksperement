5b5cd6bb55a48aa68486cf2270d5b4e5383ba220
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-08 00:12:14 +0500

    recursion function

---
+++ 15-lesson/fifteen.js
@@ -0,0 +1,35 @@
+// let b = 0;
+// const getSum = (a) => {
+//     if (a === 1) return b += 1
+//     else {
+//         b += a
+//         getSum(a-1)
+//     }
+// }
+// getSum(4)
+// console.log(b);
+// ===========================================
+// ==========Faktorial solving==================
+// let b = '';
+// const math = (a) => {    
+//     if (a === 1) return b += 1
+//     else {         
+//         b += a             
+//         return math(a - 1)
+//     }    
+// }
+
+// math(7)
+// console.log(eval(b.split('').join('*')));
+// ============================================
+let a = [2, 3]
+
+console. log(a.at[1]);
+// const fib = (b) => {
+//     for(i in a) {
+//         // console.log([a.length-2], [a.length-1]);
+//         // let c = a.length-2 + a[i-1]
+//         // console.log(c);
+//     }
+// }
+// fib()
