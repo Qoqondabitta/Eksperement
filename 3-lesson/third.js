5de68dc7f800fcfa21d86b31d2b09decec97d150
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-10 15:24:46 +0500

    if, else

---
+++ 3-lesson/third.js
@@ -0,0 +1,60 @@
+// ==============================if================================
+// let a = 'Muslim'
+
+// if(a !== 'Atheist') {
+//     console.log(a);
+// }
+// =======================if else and else=========================
+// let number = 18
+
+// if (number % (2 || 3 || 5) !== 0) {
+//     console.log(`${number} is tub`);
+// } else if (number === (2 || 3 || 5)) {
+//     console.log(`${number} is tub`);
+// } else {
+//     console.log(`${number} is plain number`);
+// }
+// =======================finding tub number=======================
+// let number = 2
+// if ((number % (2 || 3 || 5) !== 0)||(number === (2 || 3 || 5))) 
+// console.log(`${number} is a tub number`);
+// else console.log(`$number} is a plain number`);
+// =====================Homework of if=============================
+// =======================first task===============================
+// if ("0") {
+//     console.log('Hello');
+// }
+// ======================second task===============================
+// let answer = 'Vanilla JavaScript'
+
+// if (answer === "ECMAScript") {
+//     console.log('Right, well done!');
+// } else {
+//     console.log(`Wrong, don't be sad,
+//     maybe next time you'll be right.`);
+// }
+// ======================third task================================
+// let number = 2
+
+// if (number > 0) {
+//     console.log(1);
+// } else if (number < 0) {
+//     console.log(-1);
+// } else {
+//     console.log(0);
+// }
+// ====================fourth task==================================
+// let a = -9
+// let b = 10
+// let result;
+// (a + b < 4) ? result = 'Below':result='Over'
+// console.log(result);
+// =========================fifth task==============================
+// let message;
+// let login='Employee'
+// login==='Employee' 
+// ? message = "Hello"
+// :login==="Director" ? message="Greetings"
+// :login==='' ? message='No login' : message=''
+
+// console.log(message);
