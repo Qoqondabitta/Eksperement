ebc02822865b0bdd14e30428a013d97b74a2ff37
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-11 05:51:16 +0500

    switch, for loop

---
+++ 4-lesson/fourth.js
@@ -0,0 +1,21 @@
+// for (i = 2; i < 10; i++) {
+//     console.log(`
+// ${i}-integer`);
+//     for (j=1;j<11;j++) {
+//         console.log(`
+// ${i}*${j}=${i*j}`);
+//     }
+// }
+// =================switch======================
+// let a = 'cd'
+
+// switch (a) {
+//     case 'b':
+//         console.log('second');
+//         break;
+//     case 'c':
+//         console.log('third');
+//         break;
+//     default: console.log('none');
+//         break;
+// }
