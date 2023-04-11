36075648303ae00bfdd3fb1d15fe00e0dfb9876d
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-11 14:34:33 +0500

    apply, call, bind

---
+++ 19-lesson/nineteen.js
@@ -0,0 +1,16 @@
+// let a = {
+//     name : 'John',
+//     surname : 'Smith'
+// }
+
+// function getFullname(a, b, c){
+//     console.log(a, b, c);
+//     console.log(`${this.name} ${this.surname}`);
+// }
+
+// getFullname.call(a, 'hey', 'you', `what's up`)
+// getFullname.apply(a, ['hey', 'you', `what's up`])
+
+// let bind = getFullname.bind(a, 'Hey,', 'you', `what's up?`)
+
+// bind()
