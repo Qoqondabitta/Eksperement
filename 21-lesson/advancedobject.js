1a7c3afff11e03c138fa59f396478f1ad2b9d4b4
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-12 15:11:32 +0500

    object flags

---
+++ 21-lesson/advancedobject.js
@@ -0,0 +1,21 @@
+// let a = {
+//     name : 'web-brain',
+//     title : 'academy', 
+//     last : 'IT Center'
+// }
+
+// // console.log(Object.getOwnPropertyDescriptors(a));
+// Object.defineProperty(a, 'name', {writable:false})
+// Object.defineProperty(a, 'name', {configurable:false})
+// Object.defineProperty(a, 'name', {enumerable:false})
+// a.name = 'PDP'
+// console.log(a.name);
+// ===================================================================
+// delete a.name        // it is not supposed to be deleted at all
+// console.log(a.name); // it is not supposed to be deleted at all
+// ====================================================================
+// for(i in a) {           // it can not be looped when flag as  
+//     console.log(a[i]);  // enumerable is false, therefore there is no
+// }                       // object key like name in node js
+// =====================================================================
+// console.log(Object.getOwnPropertyDescriptor(a, 'name'));
