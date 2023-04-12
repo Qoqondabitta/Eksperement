d5a97940eb9f21bcf2f1f6f338027e4933e41376
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-12 15:12:41 +0500

    ddoc attack

--- 20-lesson/ddoc.js
+++ 20-lesson/ddoc.js
@@ -7,23 +7,73 @@
     // },0)
     
 // }
-function getData(){
-    return fetch('https://jsonplaceholder.typicode.com/users').then((res) => 
-    res.json()
-    );   
+// function getData(){
+//     return fetch('https://jsonplaceholder.typicode.com/users').then((res) => 
+//     res.json()
+//     );   
+// }
+
+
+// function getCached(func) {
+//     let cache = new Map()
+//     return function(x){
+//         if(cache.has(x)) {
+//             console.log(cache.get(x));
+//             return cache.get(x)
+//         }
+//         let res = func()
+//         return cache.set(x, res)
+//     }
+// }
+
+// getData = getCached(getData)
+
+
+
+function getData (){
+    fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+    res.json())
 }
 
-
-function getCached(func) {
+function getCached(func){
     let cache = new Map()
     return function(x){
-        if(cache.has(x)) {
+        if (cache.has(x)) {
             console.log(cache.get(x));
             return cache.get(x)
         }
         let res = func()
-        return cache.set(x, res)
+        return cache.get(x, res)
     }
 }
 
+
 getData = getCached(getData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
