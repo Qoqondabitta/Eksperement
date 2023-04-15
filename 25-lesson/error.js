38f7e256546eebd7201a8107ec5027ecbedf3192
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-16 03:11:10 +0500

    error handling

---
+++ 25-lesson/error.js
@@ -0,0 +1,71 @@
+// try {
+//     console.log(number);
+// } catch  {
+//     console.log('Ups, went something wrong');
+// }
+// ===============================================
+// try {
+//     console.log(number);
+// } catch (error){
+//     console.log(error);
+// }
+
+// try {
+//     console.log(number);
+// } catch (error){
+//     console.log(error.message);
+// }
+//===============================================
+// try{
+//     class a {
+//     name = 'me'
+// }
+// class b extends a {
+//     super(name)
+//     getName(){
+//         console.log(this.name);
+//     }
+// }
+
+// let c = new b()
+// c.getName()
+// } catch(error){
+//     console.log(error.message);
+// }
+// ======================================================
+// try {
+//     console.log(number);
+// } catch (error){
+//     console.log(error.name);
+// }
+// ==================================
+// try {
+//     JSON.parse('{name:"me"}')
+// } catch (error){
+//     console.log(error.name);
+//     console.log(error.message);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
