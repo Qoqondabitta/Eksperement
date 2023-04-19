a509b13e548f85e8f696e62b07fbdb9599caf452
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-04-20 02:52:50 +0500

    reviewing past themes

--- 26-lesson/generator.js
+++ 26-lesson/generator.js
@@ -20,10 +20,61 @@
 // }
 // ======================array spread operator===================
 // console.log([...generator]);
-try {
-    console.log(number);
-    console.log(a);
-} catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+// try {
+//     console.log(number);
+//     console.log(a);
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+// }
+// class a {
+//     getInfo(){
+//         console.log('Here is child');
+//     }
+// }
+// class b extends a {
+//     getChild(){
+//         console.log('Here is parent');
+//     }
+//     getBoth(){
+//         this.getChild()
+//         super.getInfo()
+//     }
+// }
+// let c = new b()
+// c.getBoth()
+// ==================================================
+class a {
+    #name = 'me'    
+    set getPrivate(value){
+        this.#name = value
+        console.log(value);
+    }
+    get getPrivate(){
+        console.log(this.#name);
+    }
 }
+let b = new a()
+b.getPrivate
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
