48f5797adb7e2858e03950c0c2d8d900ec432c57
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-09 19:32:01 +0500

    just learning new topics as module

---
+++ 27-lesson/mock.js
@@ -0,0 +1,130 @@
+// export let cars = [
+//     {id:1, year:1998, engine:1, brand:`Spark`},
+//     {id:2, year:2005, engine:1.2, brand:`Tahoe`},
+//     {id:3, year:2010, engine:1.6, brand:`Traverse`},
+//     {id:4, year:2010, engine:1.5, brand:`Cobalt`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`},
+//     {id:5, year:2012, engine:2, brand:`Malibu`},
+//     {id:6, year:2000, engine:1.2, brand:`Truck`},
+//     {id:7, year:2018, engine:2.4, brand:`Tracker`}
+// ]
+// export let list = [0,9]
+// const mentors = ()=>{
+//     console.log('Hi mentors');
+// }
+// const students = ()=>{
+//     console.log('Hi students');
+// }
+// export {mentors, students}
+// export default students
+// =================================================
+export let cars = [
+    {id:1, year:1998, engine:1, brand:`Spark`},
+    {id:2, year:2005, engine:1.2, brand:`Tahoe`},
+    {id:3, year:2010, engine:1.6, brand:`Traverse`},
+    {id:4, year:2010, engine:1.5, brand:`Cobalt`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`},
+    {id:5, year:2012, engine:2, brand:`Malibu`},
+    {id:6, year:2000, engine:1.2, brand:`Truck`},
+    {id:7, year:2018, engine:2.4, brand:`Tracker`}
+]
+export let list = [0, 1, 2, 3]
+export let name = {name : 'webAcademy'}
+const mentors  = ()=>{
+    console.log('hi mentors');
+}
+const students  = ()=>{
+    console.log('hi students');
+}
+export {students, mentors}
+export default students
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 27-lesson/prototype.js
@@ -0,0 +1,8 @@
+const mentors = ()=>{
+    console.log('Hi mentors');
+}
+const students = ()=>{
+    console.log('Hi students');
+}
+students()
+mentors()
