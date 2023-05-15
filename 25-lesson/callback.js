16e923e0fe8879cf87206f53552bcc3d3d90af93
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-15 19:30:31 +0500

    call back

--- 25-lesson/callback.js
+++ 25-lesson/callback.js
@@ -1,19 +1,70 @@
-import {player} from "./mock1.js"
+// import {player} from "./mock1.js"
+let player = {
+    Sterling: {
+        club : 'Chelsea',
+        number : '10',
+        trophy : 'EPL'
+    }, 
+    Stones : {
+        club : 'Manchester city',
+        number : '5',
+        trophy : 'EPL'
+    }
+}
 
-function check (lg, pw, callback){
+console.log('started');
+let  a = new Promise((resolve, reject)=>{
     setTimeout(() => {        
-        if (player.Sterling.club === lg && player.Sterling.number === pw) {
-            return callback(`Hitherto Sterling won ${player.Sterling.trophy} and plays under the number 10`)
+        if (player.Sterling.club === 'Chelsea' && player.Sterling.number === '10') {
+        return resolve(`Hitherto Sterling won ${player.Sterling.trophy} and plays under the number 10`)
         } else {
-            return callback(`Ouh! Sorry, but there is no such a player with the data provided`)
+        return reject(`Ouh! Sorry, but there is no such a player with the data provided`)
         }
     }, 3000);
-}
-console.log('started')
-check('Chelsea', '10', (final)=>{
-console.log(final)
-console.log('finished')
 })
+const final = async()=>{
+    let b = await a
+    console.log(b);
+    console.log('finished');
+}
+final()
+// let all = Promise.all([
+//     new Promise((resolve, reject)=>{
+//         setTimeout(() => {        
+//             if (player.Sterling.club === 'Chelsea' && player.Sterling.number === '10') {
+//             return resolve(`Hitherto Sterling won ${player.Sterling.trophy} and plays under the number 10`)
+//             } else {
+//             return reject(`Ouh! Sorry, but there is no such a player with the data provided`)
+//             }
+//         }, 3000);
+//     }),
+//     new Promise((resolve, reject)=>{
+//         setTimeout(() => {        
+//             if (player.Sterling.club === 'Chelsea' && player.Sterling.number === '10') {
+//             return resolve(`Hitherto Sterling won ${player.Sterling.trophy} and plays under the number 10`)
+//             } else {
+//             return reject(`Ouh! Sorry, but there is no such a player with the data provided`)
+//             }
+//         }, 3000);
+//     })
+// ])
+// all.then((res)=>console.log(res)).catch((error)=>{console.log(error)}).finally(()=>console.log('finished'))
+// a.then((res)=>console.log(res)).catch((error)=>{console.log(error)}).finally(()=>console.log('finally'))
+// =========================================================================================================
+// function check (lg, pw, callback){
+//     setTimeout(() => {        
+//         if (player.Sterling.club === lg && player.Sterling.number === pw) {
+//             return callback(`Hitherto Sterling won ${player.Sterling.trophy} and plays under the number 10`)
+//         } else {
+//             return callback(`Ouh! Sorry, but there is no such a player with the data provided`)
+//         }
+//     }, 3000);
+// }
+// console.log('started')
+// check('Chelsea', '10', (final)=>{
+// console.log(final)
+// console.log('finished')
+// })
 // let receive = check('Chelsea', '10')
 // console.log(receive)
-console.log('finished')
+// console.log('finished')
