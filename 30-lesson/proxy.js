4f8387fe3696e39f3a7e785320182cdc2f2c04a5
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-19 21:21:38 +0500

    proxy

---
+++ 30-lesson/proxy.js
@@ -0,0 +1,25 @@
+let player = {
+    club : "Barcelona",
+    number : "10",
+    _name : 'Lionel' 
+}
+// let proxy = new Proxy(player, {
+//     get(target, property){
+//         // if(property.startsWith('_')) return false
+//         return target[property]
+//         // return 'me'
+//         // console.log(target, property);
+//         // return 'hey babe'
+//     }
+// })
+let proxy = new Proxy(player, {
+    set(target, property, value){
+        return value
+    }
+})
+proxy.trophy = 'EPL'
+proxy._name = 'Harry'
+console.log(proxy);
+// console.log(proxy.club);
+// console.log(proxy.club);
+// console.log(proxy.trophy);
