4a02f36fa7ff88cb8da647ab0c97e5fa75213d1f
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-20 12:08:54 +0500

    recursion

--- 15-lesson/fifteen.js
+++ 15-lesson/fifteen.js
@@ -22,9 +22,9 @@
 // math(7)
 // console.log(eval(b.split('').join('*')));
 // ============================================
-let a = [2, 3]
+// let a = [2, 3]
 
-console. log(a[a.length-1]);
+// console. log(a[a.length-1]);
 // const fib = (b) => {
 //     for(i in a) {
 //         // console.log([a.length-2], [a.length-1]);
@@ -33,3 +33,69 @@ console. log(a[a.length-1]);
 //     }
 // }
 // fib()
+// ======================================================================
+// let count = 0
+// function calculate(number) {
+//     if (number === 1) return count += 1
+//     else return count += number + calculate(number-1)
+// }
+// calculate(3)
+// console.log(count);
+// =============================================================
+// let company = {
+//     web : [
+//         {name : 'John', salary : 1000},
+//         {name : 'Peter', salary : 2000}
+//     ],
+//     na : {
+//         frontend : [
+//             {name : 'Chris', salary : 3500},
+//             {name : 'Smith', salary : 10000}
+//         ],
+//         backend : [
+//             {name : 'Harry', salary : 15000},
+//             {name : 'Mike', salary : 20000}
+//         ]
+//     }
+// }
+// function calculate(company){
+//     if (Array.isArray(company)) {
+//         return company.reduce((sum, current)=> sum + current.salary, 0)
+//     } else {
+//         let count = 0;
+//         for(let value of Object.values(company)){
+//             count += calculate(value)
+//         }
+//         return count
+//     }
+// }
+// console.log(calculate(company))
+// ==============================================================================
+// let res = 0
+// function calculate(number){
+//     if (number===1) return res += 1
+//     else return res += number + calculate(number-1)
+// }
+// calculate(3)
+// console.log(res); 
+// ==========================================================
+// let res = 0
+// function calculate(number){
+//     if (number===1) return res = number * 1
+//     else return res = number * calculate(number-1)
+// }
+// calculate(4)
+// console.log(res); 
+// =========================================================
+//0 1 1 2 3 5 8 13 
+// console.log(a);
+let a = [0, 1]
+function calculate(stop){
+    if(stop) return a[stop]
+    else {
+        let b = a.at(-1) + a.at(-2)
+        let c = a.push(b)
+        return calculate(a.length+1)
+    }
+}
+console.log(calculate(4))
