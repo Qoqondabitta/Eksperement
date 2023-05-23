7feb80bb07c0b06afe6400bf80d79e6789f741e8
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-23 09:33:06 +0500

    immediately invoked and named function expression

--- 17-lesson/seventeen.js
+++ 17-lesson/seventeen.js
@@ -4,28 +4,51 @@
 // {
 //     var a = 30
 // }
-// pasdagi variable yuqoridagi variablega tasir qilib ornini 
-// egalavoladi, oldinlari(let chiqiwidan oldin) buni oldini oliw 
-// uchun IIFE bor bogan, bu var ni local  qilib uni faqat scope ichida
-// iwlatsa boladigan qib qoyadi
-// IIFE ni formulasi : 
+// Immediately invoked function makes var variable local like let
+// and we can use the var only inside the scope to which it belongs
+// this can be solution for reassignment of var variable which occurs 
+// when you use the same name of variable for two information.
+// the formula of immediately invoked function : 
 // (()=>{
 //     var a = 'b'
 // })()
 // console.log(a);
-// mana mana bu holatda node js topomadi var ni chunki u endi global 
-// emas local bob qoldi
+// in this case node js could not find the a variable outside the scope cause
+//  it is now officially sealed and not global(local)
 // =======================NFE==============================
 // const moment = function test() {
     // test.local = 'web'
 // }
 // moment()
 // console.log(moment.local);
-// NFE bzga function ga ikinchi nom biriktiradi qaysiniki biz faqat
-// functionni block scope ichidagina chaqirolamiz yo
-// iwlatolamiz tawqarida iwlatsa eror beradi
+// named function expression is the method that can make two name for one function 
+// and the second one can be used only inside of the block scope, if it is used outside
+//  the block scope then the error will happen
 // ========================new Function===================
 // new Function was the method of creating function before the function expression, 
 // declaration and arrow function
 // let a = new Function(`console.log('hey')`)
 // a()
+// =========================================================================================
+// (()=>{
+//     var a = 'me'
+// })()
+// console.log(a);
+// function a(){
+//     var b = 'c'
+// }
+// console.log(b);
+// ======================================================================================
+// (
+//     () =>{
+//     var a = 'b'
+// })()
+// console.log(a);
+// ==============================================================================================
+// const calculate = function calculate2(number=10){
+//     if (number === 1) return console.log(number += 1)
+//     else return number += number + calculate2(number-1)
+// }
+// calculate(10)
+// let a = new Function('a', "b", 'return console.log(a+b)')
+// a(1, 9)
