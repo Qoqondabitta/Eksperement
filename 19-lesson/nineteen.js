06b692581a0dec65ead0edcba4ebec5483c5679e
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-23 09:07:22 +0500

    apply, call and  bind

--- 19-lesson/nineteen.js
+++ 19-lesson/nineteen.js
@@ -15,16 +15,29 @@
 
 // bind()
 // ===========================================================
-let a = {
-    name : 'John',
-    surname : 'Smith'
-}
-let b = {
-    name : 'John',
-    surname : 'Smith'
-}
-function getFullName(a, b, c){
-    console.log(a, b, c);
-    console.log(`${this.name} ${this.surname}`);
-}
-getFullName.call({name:'Harry'}, 'hey', 'you', 'how')
+// let a = {
+//     name : 'John',
+//     surname : 'Smith'
+// }
+// let b = {
+//     name : 'John',
+//     surname : 'Smith'
+// }
+// function getFullName(a, b, c){
+//     console.log(a, b, c);
+//     console.log(`${this.name} ${this.surname}`);
+// }
+// getFullName.call({name:'Harry'}, 'hey', 'you', 'how')
+// ===============================================================================================
+// let player ={
+//     name : 'Leo',
+//     number : 10,
+//     nationality : "Argentina"
+// };
+// function getInfo(comment){
+//     console.log(`${this.name} is from ${this.nationality} and plays under the number ${this.number}`, comment);
+// }
+// getInfo.call(player, ', who won every major trophy that every professional player dreams.')
+// getInfo.apply(player, [', who won every major trophy that every professional player dreams.'])
+// let bind = getInfo.bind(player, ', who won every major trophy that every professional player dreams.')
+// bind()
