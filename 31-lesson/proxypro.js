e3e882c9d82f4faacd302c0dd2b31248af8f63cc
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-24 19:30:55 +0500

    proxy advanced

---
+++ 31-lesson/proxypro.js
@@ -0,0 +1,33 @@
+let a = {name : 'John', _password : 'unknown', age : 17}
+let b = {
+    from : 1,
+    to : 10
+}
+let proxy = new Proxy(b, {
+    has(b, property){
+        return b.from <= property && b.to >= property
+    }
+})
+// Reflect.set(b, 'to', '9') // false
+b.to = 9 // true
+console.log(11 in proxy);
+// console.log(Object.isExtensible(a));
+// Reflect.set(a, 'title', 'student')
+// Reflect.ownKeys(a, Object.keys(a).filter((value)=>value.includes('_')===false ? console.log(value):false))
+// Reflect.preventExtensions(a)
+// Reflect.has(a, 11)
+// console.log(Reflect.isExtensible(a));
+
+// console.log(a);
+// console.log(a);
+// console.log(Reflect.get(a, 'age'));
+// let proxy = new Proxy(a, {
+//     ownKeys(a){
+//         return Object.keys(a).filter((value) => value.includes('_') === false ? console.log(value) : false)
+//     }
+// })
+// console.log(proxy);
+
+// for(i in proxy){
+    // console.log(i);
+// }
