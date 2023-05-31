881dca9202e78fb2eff7457cfc3af965005ccacd
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-05-31 19:26:21 +0500

    DOM rudiments

---
+++ 32-lesson/DOM.js
@@ -0,0 +1,73 @@
+// alert('DOM')
+// alert(window.innerHeight)
+// alert(document.title)
+// alert(document.title = 'DOM')
+// console.log(location.href);
+// console.log(location.pathname);
+// console.log(document);
+// console.log(window);
+// =======================WINDOW===================================
+// ======DOM=======   ========BOM======   ====JavaScript======
+// Document           navigate            function 
+//                    location            object
+//                                        array
+// ====================================================================
+// alert('HELLO')
+// alert(window.innerHeight)
+// alert(window.innerWidth)
+// alert(document.title)
+// alert(document.title= 'DOM')
+// console.log(location.href);
+// console.log(location.pathname);
+// console.log(document);
+// console.log(window);
+// let test = document.getElementsByTagName('ul')[0]
+// let a = document.getElementsByTagName('ul')
+// console.log(test.children);
+// console.log(test.childNodes);
+// console.log(test.firstChild);
+// console.log(test.firstElementChild);
+// console.log(test.lastElementChild);
+// console.log(test.firstElementChild.nextElementSibling);
+// console.log(test.lastElementChild.previousElementSibling);
+// console.log(test.lastElementChild.previousElementSibling.previousElementSibling);
+// let tag = document.getElementsByTagName('h1')[0]
+// console.log(tag.style.background = 'red');
+// let name = document.getElementsByClassName('hold')[0]
+// console.log(name.style.background='yellow'); 
+// let id = document.getElementById('me')
+// console.log(id.style.background='black');
+let qr = document.querySelector('#he')
+qr.innerHTML = "<h1>Hey</h1> "
+
+console.log(qr.innerHTML);
+console.log(qr.innerText);
+console.log(qr.textContent);
+// console.log(qr.style.background='black');
+// let all = document.querySelectorAll('ul')
+// console.log(all.style.background='black');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
