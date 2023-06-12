87d53d7668fe22eaabb235f06a3c2dcf094518f3
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-06-12 12:01:16 +0500

    dom

---
+++ 32-lesson/home.js
@@ -0,0 +1,26 @@
+const hold = document.getElementById("imgHolder")
+const img1 = document.querySelector('#imgBox1')
+const img2 = document.querySelector('#imgBox2')
+const img3 = document.querySelector('#imgBox3')
+const img4 = document.querySelector('#imgBox4')
+const title = document.querySelector('#h1')
+console.log(img1.style.width = '250px',);
+console.log(img2.style.width = '250px',);
+console.log(img3.style.width = '250px',);
+console.log(img4.style.width = '250px',);
+console.log(img1.style.height = '250px');
+console.log(img2.style.height = '250px');
+console.log(img3.style.height = '250px');
+console.log(img4.style.height = '250px');
+console.log(hold.style.display = "flex");
+console.log(img1.style.margin = '80px 0px auto 200px');
+console.log(img2.style.margin = '80px 0px auto 10px');
+console.log(img3.style.margin = '80px 0px auto 10px');
+console.log(img4.style.margin = '80px 0px auto 10px');
+console.log(hold.padding = '200px');
+console.log(title.style.margin = '150px 0px auto 650px');
+console.log(title.style.color = 'red');
+let c = 'font-size'
+console.log(title.c = '150px');
+// console.log(ar);
+console.log(hold.style.gap = '10px');
