783d98965197e301c31a9830dbcb36ace4afe8ff
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-06-14 12:18:09 +0500

    before, after, appenf, prepend

--- 32-lesson/home.js
+++ 32-lesson/home.js
@@ -17,10 +17,30 @@ console.log(img1.style.margin = '80px 0px auto 200px');
 console.log(img2.style.margin = '80px 0px auto 10px');
 console.log(img3.style.margin = '80px 0px auto 10px');
 console.log(img4.style.margin = '80px 0px auto 10px');
-console.log(hold.padding = '200px');
+// console.log(hold.padding = '200px');
 console.log(title.style.margin = '150px 0px auto 650px');
 console.log(title.style.color = 'red');
-let c = 'font-size'
-console.log(title.c = '150px');
-// console.log(ar);
+// let c = 'font-size'
+// console.log(title.c = '150px');
+// // console.log(ar);
 console.log(hold.style.gap = '10px');
+// =========================Dom | 2 |===============================
+// let title = document.querySelector('h1')
+// console.log(title.hasAttribute('type'));
+// console.log(title.getAttribute('id'));
+// console.log(title.removeAttribute('id'));
+// console.log(title.setAttribute('class', 'final'));
+// console.log(title.createAttribute('class'));
+// console.log(title);
+// let txt = document.createTextNode('uk')
+// dv.innerHTML = txt
+// dv.setAttribute('id', txt)
+const dv = document.createElement('div')
+dv.innerHTML = "c"
+const holder = document.querySelector('.example')
+holder.prepend(dv)
+holder.append(dv)
+holder.before(dv)
+holder.after(dv)
+// console.log(holder.append(dv));
+// console.log(dv);
