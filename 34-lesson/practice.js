47db61ded6a28a456e4248b01687873cd3f33414
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2023-06-18 00:00:20 +0500

    carusel

--- 34-lesson/practice.js
+++ 34-lesson/practice.js
@@ -29,24 +29,36 @@ const prev = document.querySelector("#previous")
 //         }
 //     }
 // }
+const names= document.querySelector("#names")
+const title = document.querySelectorAll('.name')
 function buttonAction(type){
     for(img=0;img<image.length;img++){
         if(image[img].getAttribute('class').includes('active')){
             if(type==='next'){
-            if(image[image.length-1].getAttribute('class').includes('active')){
+                image[img].classList.remove('active')
+                if(img===image.length-1){
                     image[0].classList.add('active')
-                    image[image.length-1].classList.remove('active')
                 } else {
                     image[img+1].classList.add('active')
-                    image[img].classList.remove('active')
+                }
+                title[img].classList.remove('active')
+                if(img===image.length-1){
+                    title[0].classList.add('active')
+                } else {
+                    title[img+1].classList.add('active')
                 }
             } else {
+                image[img].classList.remove('active')
                 if(img === 0){
-                    image[img].classList.remove('active')
                     image[image.length-1].classList.add('active')
                 } else {
                     image[img-1].classList.add('active')
-                    image[img].classList.remove('active')
+                }
+                title[img].classList.remove('active')
+                if(img === 0){
+                    title[image.length-1].classList.add('active')
+                } else {
+                    title[img-1].classList.add('active')
                 }
             }
             break
@@ -56,3 +68,18 @@ function buttonAction(type){
 } 
 next.addEventListener('click', ()=>buttonAction('next'))
 prev.addEventListener('click', ()=>buttonAction('prev'))
+// for(i=0;i<image.length;i++){
+//     if(image[i].getAttribute('class').includes('active')){
+//         title.classList.add('active')
+//     }
+// }
+for(i=0;i<title.length;i++){
+    title[i].addEventListener('click', ()=>{
+        for(j=0;j<title.length;j++){
+            j.classList.remove('active')
+            image[j].classList.remove('active')
+        }
+        title[i].classList.add('active')
+        image[i].classList.add('active')
+    })
+}
